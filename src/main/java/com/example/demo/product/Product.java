fa2dc568bd74f13e97dc42fe9c254aa043b07cae
Durmuş Demirtaş <demirtasdurmus@gmail.com>
2024-07-19 19:41:56 +0300

    feat: add spring boot dev tools package to enable hot reloading

--- src/main/java/com/example/demo/product/Product.java
+++ src/main/java/com/example/demo/product/Product.java
@@ -1,13 +1,13 @@
 package com.example.demo.product;
 
 public class Product {
-  private int lastId = 1;
+  private int last_id = 1;
   private int id;
   private String name;
 
   public Product(String name) {
     this.name = name;
-    this.id = lastId++;
+    this.id = last_id++;
   }
 
   public int getId() {
